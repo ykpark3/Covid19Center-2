f9682358839992494a571181a461cf9c8cf633da
taesunpark <tovbskvb@daum.net>
2020-10-28 16:52:01 +0900

    [add] project start

---
+++ app/src/main/java/org/androidtown/covid19center/MainActivity.java
@@ -0,0 +1,14 @@
+package org.androidtown.covid19center;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
