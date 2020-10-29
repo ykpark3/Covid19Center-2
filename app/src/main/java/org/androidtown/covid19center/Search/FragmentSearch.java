dc88879ec3a01935f55a218d55397876c9ec1278
taesunpark <tovbskvb@daum.net>
2020-10-29 22:42:56 +0900

    검색 엑티비티 안에 EditText 추가

--- app/src/main/java/org/androidtown/covid19center/Search/FragmentSearch.java
+++ app/src/main/java/org/androidtown/covid19center/Search/FragmentSearch.java
@@ -4,16 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.androidtown.covid19center.R;
 
 public class FragmentSearch extends Fragment {
 
     private View view;
+    private TextView search_textView;
+
+
 
     @Nullable
     @Override
@@ -21,6 +27,26 @@ public class FragmentSearch extends Fragment {
     {
         view = inflater.inflate(R.layout.fragment_search,container,false);
         return view;
+    }
+
+    @Nullable
+    @Override
+    public void onStart() {
+
+        search_textView = view.findViewById(R.id.searchBox);
+        super.onStart();
+        setSearchingBox();
+    }
+
+
+    public void setSearchingBox(){
+        search_textView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/org/androidtown/covid19center/Search/SearchActivity.java
@@ -0,0 +1,16 @@
+package org.androidtown.covid19center.Search;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.androidtown.covid19center.R;
+
+public class SearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+}
