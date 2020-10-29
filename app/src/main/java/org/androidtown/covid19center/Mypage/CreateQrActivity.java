2688e745d73a2942583ee9df9db37e50dfa9fbe6
codns1119 <codns1119@naver.com>
2020-10-29 15:15:19 +0900

    [add] CreateQrActivity 생성

---
+++ app/src/main/java/org/androidtown/covid19center/Mypage/CreateQrActivity.java
@@ -0,0 +1,7 @@
+package org.androidtown.covid19center.Mypage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateQrActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/org/androidtown/covid19center/Search/FragmentSearch.java
+++ app/src/main/java/org/androidtown/covid19center/Search/FragmentSearch.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import org.androidtown.covid19center.R;
 
@@ -20,6 +22,17 @@ public class FragmentSearch extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.fragment_search,container,false);
+
+        Button button_qr = view.findViewById(R.id.button_qr);
+
+        button_qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Qr코드 생성 activity로 intent
+            }
+        });
+
+
         return view;
     }
 
