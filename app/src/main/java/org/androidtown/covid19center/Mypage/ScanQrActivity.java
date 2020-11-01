98d89235cb9b644273b87b82c86d5c5a1d32f2c8
codns1119 <codns1119@naver.com>
2020-11-01 13:37:18 +0900

    [modify] QR코드 스캔 화면 세로로 변경

--- app/src/main/java/org/androidtown/covid19center/Mypage/ScanQrActivity.java
+++ app/src/main/java/org/androidtown/covid19center/Mypage/ScanQrActivity.java
@@ -13,13 +13,17 @@ import com.google.zxing.integration.android.IntentResult;
 import org.androidtown.covid19center.R;
 
 public class ScanQrActivity extends AppCompatActivity {
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qr);
 
-        new IntentIntegrator(this).initiateScan();
+//        new IntentIntegrator(this).initiateScan();
+        qrScan = new IntentIntegrator(this);
+        qrScan.setOrientationLocked(false); // default가 세로모드인데 휴대폰 방향에 따라 가로, 세로로 자동 변경됩니다.
+        qrScan.initiateScan();
     }
 
     @Override
