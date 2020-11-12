4ec7ebb5f3f60f1c302edcc6790ff7f2f28c6f78
taesunpark <tovbskvb@daum.net>
2020-11-12 17:51:32 +0900

    [add] 지도 들어갔을 때 현재 위치 보여주기

--- app/src/main/java/org/androidtown/covid19center/Map/MapActivity.java
+++ app/src/main/java/org/androidtown/covid19center/Map/MapActivity.java
@@ -9,11 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.overlay.InfoWindow;
 import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
 
 import org.androidtown.covid19center.DataBase.AppDatabase;
 import org.androidtown.covid19center.R;
@@ -25,9 +27,12 @@ import java.util.List;
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     MapFragment mapFragment;
+    private static final int LOCATTION_PERMISSION_REQUEST_CODE = 1000;
     private AppDatabase db;
     private ArrayList<ClinicItem> clinicDataList; // 진료소 담을 리스트 생성
     private int number;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,18 +42,42 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         setContentView(R.layout.fragment_map);
         FragmentManager fm = getSupportFragmentManager();
         mapFragment = (MapFragment) fm.findFragmentById(R.id.map);
+        Log.d("테스트","4");
 
         if (mapFragment == null) {
             mapFragment = MapFragment.newInstance();
             fm.beginTransaction().replace(R.id.map, mapFragment).commit();
+            Log.d("테스트","6");
         }
-
+        Log.d("테스트","5");
+        locationSource = new FusedLocationSource(this, LOCATTION_PERMISSION_REQUEST_CODE);
         mapFragment.getMapAsync(MapActivity.this);
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+        Log.d("테스트","2");
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+                Log.d("테스트","1");
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
     }
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-
+        Log.d("테스트","3");
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
         List<Marker> markers = new ArrayList<>();
         InfoWindow infoWindow = new InfoWindow();
 
