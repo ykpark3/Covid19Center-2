b20fd470d903096051f8802d195f129bcbe352a3
codns1119 <codns1119@naver.com>
2020-11-13 16:15:06 +0900

    [add] 자가진단 결과  레이아웃

--- app/src/main/java/org/androidtown/covid19center/SelfCheck/FragmentSelfCheck.java
+++ app/src/main/java/org/androidtown/covid19center/SelfCheck/FragmentSelfCheck.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import org.androidtown.covid19center.R;
 
@@ -15,12 +16,31 @@ public class FragmentSelfCheck extends Fragment {
 
     private View view;
 
+    private CheckBox fever;
+    private CheckBox muscle_ache;
+    private CheckBox cough;
+    private CheckBox sputum;
+    private CheckBox chill;
+    private CheckBox dyspnea;
+    private CheckBox sore_throat;
+
+    int check_num = 0;  //자가진단 체크한 증상 개수
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-//        view = inflater.inflate(R.layout.fragment_self_check,container,false);
-        view = inflater.inflate(R.layout.activity_questionnaire,container,false);
+        view = inflater.inflate(R.layout.fragment_self_check,container,false);
+//        view = inflater.inflate(R.layout.activity_questionnaire,container,false);
+
+        fever = view.findViewById(R.id.self_check_fever);
+        muscle_ache = view.findViewById(R.id.self_check_muscle_ache);
+        cough = view.findViewById(R.id.self_check_cough);
+        sputum = view.findViewById(R.id.self_check_sputum);
+        chill = view.findViewById(R.id.self_check_chill);
+        dyspnea = view.findViewById(R.id.self_check_dyspnea);
+        sore_throat = view.findViewById(R.id.self_check_sore_throat);
+
         return view;
     }
 }
