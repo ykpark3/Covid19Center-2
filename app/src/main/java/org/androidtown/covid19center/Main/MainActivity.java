9dd42671cf0ed5ca4edf4207bb0f26ca54f0bbcc
codns1119 <codns1119@naver.com>
2020-11-16 21:17:32 +0900

    [test] 머지 테스트2

--- app/src/main/java/org/androidtown/covid19center/Main/MainActivity.java
+++ app/src/main/java/org/androidtown/covid19center/Main/MainActivity.java
@@ -204,5 +204,6 @@ public class MainActivity extends AppCompatActivity implements OnBackPressedList
         Toast.makeText(this, "'뒤로' 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
     }
 
+
     
 }
