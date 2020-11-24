97c54f43dfb986d1f645f651de8c1d2cb5f9326f
taesunpark <tovbskvb@daum.net>
2020-11-24 16:42:51 +0900

    [update] 문진표 엑티비티에 데이터 값 세팅완료

--- app/src/main/java/org/androidtown/covid19center/SelfCheck/QuestionnarieActivity.java
+++ app/src/main/java/org/androidtown/covid19center/SelfCheck/QuestionnarieActivity.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.NumberPicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -33,8 +37,16 @@ public class QuestionnarieActivity extends AppCompatActivity implements NumberPi
     private final Calendar myCalender = Calendar.getInstance();
     private String clinicName;
     private String clinicReservationTime;
+    private String symptom_start_date;
+    private String visitedDetail;
+    private String contact_relationship;
+    private String contact_period;
+    private String entrance_date;
     private ImageButton backButton;
     private Button nextButton;
+    private RadioGroup visitedCheck_radioGroup, contact_radioGroup;
+    private Boolean isVisited, isContected, hasFever, hasMuscle_ache, hasCough, hasSputum, hasRunnyNose, hasDyspnea, hasSoreThroat;
+    private CheckBox fever_checkBox, muscle_ache_checkBox, cough_checkBox, sputum_checkBox, runny_nose_checkBox, dyspnea_checkBox, sore_throat_checkBox;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,7 +56,6 @@ public class QuestionnarieActivity extends AppCompatActivity implements NumberPi
         setIntentInfo();
     }
 
-
     private void setLayoutElement(){
         countryEditText = findViewById(R.id.questionnarie_editText_country);
         entranceDateTextView = findViewById(R.id.questionnarie_textView_date);
@@ -53,11 +64,40 @@ public class QuestionnarieActivity extends AppCompatActivity implements NumberPi
         startVirusDateTextView = findViewById(R.id.questionnarie_textView_start_date);
         nextButton = findViewById(R.id.questionnarie_button);
         backButton = findViewById(R.id.questionnarie_backButton);
+        contact_radioGroup = findViewById(R.id.questionnarie_contact_radioGroup);
+        contact_radioGroup.setOnCheckedChangeListener(contactRadioGroupButtonChangeListener);
+        visitedCheck_radioGroup = findViewById(R.id.questionnarie_visited_radioGroup);
+        visitedCheck_radioGroup.setOnCheckedChangeListener(visitedCheckRadioGroupButtonChangeListener);
+
+
+        fever_checkBox = findViewById(R.id.questionnarie_fever_radioButton_true);
+        muscle_ache_checkBox = findViewById(R.id.questionnarie_muscle_ache_radioButton_true);
+        cough_checkBox = findViewById(R.id.questionnarie_cough_radioButton_true);
+        sputum_checkBox = findViewById(R.id.questionnarie_sputum_radioButton_true);
+        runny_nose_checkBox = findViewById(R.id.questionnarie_runny_nose_radioButton_true);
+        dyspnea_checkBox = findViewById(R.id.questionnarie_dyspnea_radioButton_true);
+        sore_throat_checkBox = findViewById(R.id.questionnarie_sore_throat_radioButton_true);
+
+        clinicReservationTime = null;
+        symptom_start_date = null;
+        visitedDetail = null;
+        contact_relationship = null;
+        contact_period = null;
+        entrance_date = null;
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 서버에 전달
+                setCheckedInfo();
+
+                //
+                // 서버에 전송하는 코드 작성
+                //
+
+
+                //Log.d("1607", entrance_date+"\n"+isVisited+"\n"+visitedDetail+"\n"+isContected+"\n"+contact_relationship+"\n"+contact_period+"\n"+hasFever+"\n"+hasMuscle_ache+"\n"+hasCough+"\n"+hasSputum+"\n"+hasRunnyNose+"\n"+hasDyspnea+"\n"+ hasSoreThroat+"\n"+symptom_start_date+"\n"+clinicName+"\n"+clinicReservationTime);
+                Toast.makeText(getApplicationContext(), "눌림", Toast.LENGTH_SHORT).show();
+                
             }
         });
 
@@ -67,7 +107,6 @@ public class QuestionnarieActivity extends AppCompatActivity implements NumberPi
                 onBackPressed();
             }
         });
-
 
         DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -88,7 +127,6 @@ public class QuestionnarieActivity extends AppCompatActivity implements NumberPi
                 updateVirusStartLabel();
             }
         };
-
 
         entranceDateTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,6 +151,86 @@ public class QuestionnarieActivity extends AppCompatActivity implements NumberPi
 
     }
 
+    RadioGroup.OnCheckedChangeListener contactRadioGroupButtonChangeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if(checkedId == R.id.questionnarie_contact_radioButton_true){
+                isContected = true;
+            } else if(checkedId == R.id.questionnarie_contact_radioButton_false){
+                isContected = false;
+            }
+        }
+    };
+
+    RadioGroup.OnCheckedChangeListener visitedCheckRadioGroupButtonChangeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+            if(checkedId == R.id.questionnarie_visited_radioButton_true){
+                isVisited = true;
+            } else if(checkedId == R.id.questionnarie_visited_radioButton_false){
+                isVisited = false;
+            }
+
+
+        }
+    };
+
+    private void setCheckedInfo(){
+
+        if(visitedDetail.equals("")){
+            visitedDetail = null;
+        }
+        if(contact_relationship.equals("")){
+            contact_relationship = null;
+        }
+
+        if(fever_checkBox.isChecked() == true){
+            hasFever = true;
+        } else{
+            hasFever = false;
+        }
+
+        if(muscle_ache_checkBox.isChecked() == true){
+            hasMuscle_ache = true;
+        } else{
+            hasMuscle_ache = false;
+        }
+
+        if(cough_checkBox.isChecked() == true){
+            hasCough = true;
+        } else{
+            hasCough = false;
+        }
+
+        if(runny_nose_checkBox.isChecked() == true){
+            hasRunnyNose = true;
+        } else{
+            hasRunnyNose = false;
+        }
+
+        if(dyspnea_checkBox.isChecked() == true){
+            hasDyspnea = true;
+        } else{
+            hasDyspnea = false;
+        }
+
+        if(sputum_checkBox.isChecked() == true){
+            hasSputum = true;
+        } else{
+            hasSputum = false;
+        }
+
+        if(sore_throat_checkBox.isChecked() == true){
+            hasSoreThroat = true;
+        } else{
+            hasSoreThroat = false;
+        }
+
+        contact_relationship = String.valueOf(relationEditText.getText());
+        visitedDetail = String.valueOf(countryEditText.getText());
+    }
+
     private void setIntentInfo(){
         clinicName = getIntent().getExtras().getString("clinicName");
         clinicReservationTime = getIntent().getExtras().getString("clinicTime");
@@ -123,6 +241,7 @@ public class QuestionnarieActivity extends AppCompatActivity implements NumberPi
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
         entranceDateTextView.setText(sdf.format(myCalender.getTime()));
         entranceDateTextView.setTextColor(Color.BLACK);
+        entrance_date = sdf.format(myCalender.getTime());
     }
 
     private void updateVirusStartLabel(){
@@ -130,14 +249,14 @@ public class QuestionnarieActivity extends AppCompatActivity implements NumberPi
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
         startVirusDateTextView.setText(sdf.format(myCalender.getTime()));
         startVirusDateTextView.setTextColor(Color.BLACK);
+        symptom_start_date = sdf.format(myCalender.getTime());
     }
-
-
 
     @Override
     public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
         termTextView.setText(String.valueOf(newVal)+ "일");
         termTextView.setTextColor(Color.BLACK);
+        contact_period = String.valueOf(newVal);
     }
 
     public void showNumberPicker(View view, String title, String subtitle, int maxvalue, int minvalue, int step, int defvalue){
@@ -156,6 +275,5 @@ public class QuestionnarieActivity extends AppCompatActivity implements NumberPi
         newFragment.setValueChangeListener(this);
         newFragment.show(getSupportFragmentManager(), "null");
     }
-
 
 }
